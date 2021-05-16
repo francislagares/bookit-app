eb3b96d5d55a204703faf64d842c9f355a815ec4
Francis Lagares <francis.lagares@gmail.com>
2021-05-16 17:22:48 +0200

    feat(utils): create custom error handler class

---
+++ utils/errorHandler.ts
@@ -0,0 +1,9 @@
+class ErrorHandler extends Error {
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default ErrorHandler;
