a3b8b85832ecb8780e60d96012ddff2d65539f36
Francis Lagares <francis.lagares@gmail.com>
2021-05-20 20:18:46 +0200

    feat(redux): add dispatch action types

--- src/redux/actions/roomActions.ts
+++ src/redux/actions/roomActions.ts
@@ -1,31 +1,35 @@
+import { Dispatch } from 'react';
+import { IncomingMessage } from 'http';
 import axios from 'axios';
-import { NextApiRequest } from 'next';
 import absoluteUrl from 'next-absolute-url';
 import {
   ALL_ROOMS_SUCCESS,
   ALL_ROOMS_FAIL,
   CLEAR_ERRORS,
 } from '../constants/roomConstants';
+import { IAllRoomsFail, IAllRoomsSuccess, IClearErrors } from 'src/interfaces';
 
-export const getRooms = (req: NextApiRequest) => async dispatch => {
-  try {
-    const { origin } = absoluteUrl(req);
+export const getRooms =
+  (req: IncomingMessage) =>
+  async (dispatch: Dispatch<IAllRoomsSuccess | IAllRoomsFail>) => {
+    try {
+      const { origin } = absoluteUrl(req);
 
-    const { data } = await axios.get(`${origin}/api/rooms`);
+      const { data } = await axios.get(`${origin}/api/rooms`);
 
-    dispatch({
-      type: ALL_ROOMS_SUCCESS,
-      payload: data,
-    });
-  } catch (error) {
-    dispatch({
-      type: ALL_ROOMS_FAIL,
-      payload: error.response.data.message,
-    });
-  }
-};
+      dispatch({
+        type: ALL_ROOMS_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: ALL_ROOMS_FAIL,
+        payload: error.response.data.message,
+      });
+    }
+  };
 
-export const clearErrors = () => async dispatch => {
+export const clearErrors = () => async (dispatch: Dispatch<IClearErrors>) => {
   dispatch({
     type: CLEAR_ERRORS,
   });
