56664117b8a21acf61537b76341cf98936552471
Francis Lagares <francis.lagares@gmail.com>
2021-05-20 20:37:54 +0200

    feat(pages): add getServerSideProps wrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
 import Home from '../components/Home';
 import Layout from '../components/layout';
 
+import { getRooms } from '../redux/actions/roomActions';
+
+import { wrapper } from '../redux/store';
+
 const Index = () => (
   <Layout>
     <Home />
   </Layout>
+);
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  async ({ req, store }) => {
+    await store.dispatch(getRooms(req));
+  },
 );
 
 export default Index;
