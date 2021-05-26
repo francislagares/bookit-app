012970c59823f6ba796b1e56dd7b9e1a2810c9d4
Francis Lagares <francis.lagares@gmail.com>
2021-05-26 18:30:36 +0200

    feat(models): method 'save' to encrypt password when register user

--- models/user.ts
+++ models/user.ts
@@ -43,5 +43,13 @@ const userSchema = new mongoose.Schema({
   resetPasswordExpire: Date,
 });
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
 export default mongoose.models.User ||
   mongoose.model<IUser>('User', userSchema);
